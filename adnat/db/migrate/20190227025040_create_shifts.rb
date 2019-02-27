0f607551d46e5c5e453a0fc25f7d433b969a762c
PTGreenwood <peter.greenwood92@live.com.au>
2019-02-27 12:51:14 +1000

    Initial Creation of Shifts Model

---
+++ adnat/db/migrate/20190227025040_create_shifts.rb
@@ -0,0 +1,12 @@
+class CreateShifts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shifts do |t|
+      t.references :user, foreign_key: true
+      t.datetime :start
+      t.datetime :finish
+      t.int :breaklength
+
+      t.timestamps
+    end
+  end
+end
