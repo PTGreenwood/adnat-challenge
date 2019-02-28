7dfd1fea85cd1f6fa03b8860ac8ea79844e2efae
PTGreenwood <peter.greenwood92@live.com.au>
2019-02-28 11:02:43 +1000

    Finalised Serverside model validation for Users, Organisations and Shifts

--- adnat/app/models/shift.rb
+++ adnat/app/models/shift.rb
@@ -2,16 +2,14 @@ class Shift < ApplicationRecord
 
   validates :start, presence: true #Must have a start time.
   validates :finish, presence: true #Must have an end time
-  validates :breaklength, presence: true #Break length is required
+  validates :breaklength, presence: true, numericality: { greater_than_or_equal_to: 0 } #Break length is required
 
+  #Date and time validator GEM.
   validates_time :start, :between => ['12:00am', '11:59pm'] #Start time must be a valid time
   validates_time :finish, :between => ['12:00am', '11:59pm'] #End time must be a valid time
-  validates_time :finish, :after => lambda {:start}
-  #Finish time must be > than start time (at this stage. Overnight shifts may complicate things).
-
+  validates_time :finish, :after => lambda {:start} #Finish time must be > than start time (at this stage. Overnight shifts may complicate things).
+  
   #Referential Constrains
   belongs_to :user, optional: false #A shift must belong to a user.
-
-  #NOTE: Incorporate DateValidator GEM for date validation as rails doesn't seem to support it at this present time.
 
 end
--- adnat/app/models/user.rb
+++ adnat/app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    
-  #NOTE: Only 1 extra model constraints to be added. As Devise will handle most, if not all of these.
-  validates :name, presence: true #must have a name 
+  #NOTE: Only 2 model constraints to be added. As Devise will handle most, if not all of these. Especailly encrypted password
+  validates :name, presence: true, length: { minimum: 3 } #must have a name of minimum 3 letters
+  validates :email, presence: true, length: { minimum: 5 } #email must be a minimum of 5 letters - Note: Client checks will catch this first. This is to save it going into the DB
 
   #Referential Constraints
   belongs_to :organisation, optional: true #Setting optional to true, allows a user to not be a part of an organisation (such as when first registering)
